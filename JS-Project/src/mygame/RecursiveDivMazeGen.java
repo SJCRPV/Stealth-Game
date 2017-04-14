29b06f85876f720eab3c204a77bb8263af6a5724
SJCRPV <sjcrpv.games@hotmail.com>
2017-04-14 19:43:47 +0100

    Mais progresso no algoritmo. Nao estou a gostar do jMonkey

--- JS-Project/src/mygame/RecursiveDivMazeGen.java
+++ JS-Project/src/mygame/RecursiveDivMazeGen.java
@@ -8,6 +8,7 @@ package mygame;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
 
 /**
@@ -20,58 +21,115 @@ public class RecursiveDivMazeGen {
     Geometry plane;
     final int MAX_AREA_WIDTH;
     final int MAX_AREA_HEIGHT;
+    final int WALL_THICKNESS;
     int minRoomWidth;
     int minRoomHeight;
-    int doorWidth;
-    int doorHeight;
+    int doorSize;
+    boolean cutIsHorizontal;
     
-    private void carveOpening(boolean isHorizontal, int[] bisectionStartPos, int bisectionSize)
+    private boolean isCutHorizontal(float quadWidth, float quadHeight)
     {
-        //Randomize placement of opening.
-        //Delete previous object, create two objects that leave the opening
+        return quadHeight > quadWidth;
     }
     
-    private Quad[] bisectSection(Vector3f sectionCentrePos, Quad section)
+    private int randomizeCarvePlacement(int min, int max)
     {
-        //quadsToReturn[0] represents either the left or the up Quad, depending on the orientation of the bisection
-        Quad[] quadsToReturn = new Quad[2];
+        //Returns a random number that will be the central pixel in the lenght of the wall that gets carved.
+    }
+    
+    private void carveOpening(Geometry[] geos)
+    {
+        int doorCentre = randomizeCarvePlacement();
+        //Delete previous object, create two objects that leave the opening. doorSize/2 for each side
+    }
+    
+    private Geometry[] fillSection(Box[] boxesToFillWith, Vector3f sectionCentrePos)
+    {
+        Geometry[] geosToReturn = new Geometry[2];
+        
+        geosToReturn[0] = new Geometry("Box1", boxesToFillWith[0]);
+        geosToReturn[1] = new Geometry("Box2", boxesToFillWith[1]);
+        
+        if(cutIsHorizontal)
+        {
+            geosToReturn[0].setLocalTranslation(sectionCentrePos.x, sectionCentrePos.y + boxesToFillWith[0].getYExtent()/2 
+                    + WALL_THICKNESS, sectionCentrePos.z);
+            geosToReturn[1].setLocalTranslation(sectionCentrePos.x, sectionCentrePos.y - boxesToFillWith[1].getYExtent()/2 
+                    - WALL_THICKNESS, sectionCentrePos.z);
+        }
+        else
+        {
+            geosToReturn[0].setLocalTranslation(sectionCentrePos.x - boxesToFillWith[0].getXExtent()/2 - WALL_THICKNESS, 
+                    sectionCentrePos.y, sectionCentrePos.z);
+            geosToReturn[1].setLocalTranslation(sectionCentrePos.x + boxesToFillWith[1].getXExtent()/2 + WALL_THICKNESS, 
+                    sectionCentrePos.y, sectionCentrePos.z);
+        }
+        return geosToReturn;
+    }
+    
+    private Box[] bisectSection(Quad section)
+    {
+        //boxesToReturn[0] represents either the left or the up Quad, depending on the orientation of the bisection
+        Box[] boxesToReturn = new Box[2];
         if(section.getWidth() > minRoomWidth && section.getHeight() > minRoomHeight)
         {
-            //See if bisection will be vertical of horizontal
-            //Make bisection
-            //Make object to fill bisection
-            //carveOpening();
+            cutIsHorizontal = isCutHorizontal(section.getWidth(), section.getHeight());
+            if(cutIsHorizontal)
+            {    
+                boxesToReturn[0] = new Box(section.getWidth(), section.getHeight()/2 - WALL_THICKNESS, 1f);
+            }
+            else
+            {
+                boxesToReturn[0] = new Box(section.getWidth()/2 - WALL_THICKNESS, section.getHeight(), 1f);
+            }
+            boxesToReturn[1] = boxesToReturn[0];
+            
+            
         }
         
-        return quadsToReturn;
+        return boxesToReturn;
     }
     
     private void recursiveDivision(Quad area)
     {
-        
         if(area.getWidth() > minRoomWidth && area.getHeight() > minRoomHeight)
         {
-            Quad[] dividedQuads = bisectSection(area.getBound().getCenter(), area);
+            Box[] dividedBoxes = bisectSection(area);
+            Geometry[] dividedGeos = fillSection(dividedBoxes, area.getBound().getCenter());
+            carveOpening(dividedGeos);
             
-            recursiveDivision(dividedQuads[0]);
-            recursiveDivision(dividedQuads[1]);
+            recursiveDivision(dividedBoxes[0]);
+            recursiveDivision(dividedBoxes[1]);
         }
     }
     
     public void generateMaze()
     {
-        //Gives centre of Geometry
-        //plane.getLocalTransform().getTranslation();
+        /*Gives centre of Geometry
+        plane.getLocalTransform().getTranslation();*/
         recursiveDivision(new Quad(MAX_AREA_WIDTH, MAX_AREA_HEIGHT));
     }
     
-    public RecursiveDivMazeGen(int areaWidth, int areaHeight, int newMinRoomWidth, int newMinRoomHeight, int newDoorWidth, int newDoorHeight)
+    //(0,0) will be on the bottom left
+    private void givePlaneSaneCoordinates(Geometry plane)
+    {
+        Vector3f temp = plane.getLocalTranslation();
+        temp.x = 0 + MAX_AREA_WIDTH/2;
+        temp.y = 0 + MAX_AREA_HEIGHT/2;
+        
+        plane.setLocalTranslation(temp);
+    }
+    
+    public RecursiveDivMazeGen(int areaWidth, int areaHeight, int newMinRoomWidth, int newMinRoomHeight, int doorSize, 
+            int wallThickness)
     {
         MAX_AREA_WIDTH = areaWidth;
         MAX_AREA_HEIGHT = areaHeight;
+        WALL_THICKNESS = wallThickness;
         minRoomWidth = newMinRoomWidth;
         minRoomHeight = newMinRoomHeight;
         plane = new Geometry("Floor", new Quad(MAX_AREA_WIDTH, MAX_AREA_HEIGHT));
+        givePlaneSaneCoordinates(plane);
         generatedMaze = new Node();
     }
 }
